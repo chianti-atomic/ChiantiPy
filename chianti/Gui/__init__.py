49371c74ec86cf6c8fd555d3a796890197ad63d6
Will Barnes <wtb2@rice.edu>
2016-03-22 14:55:49 -0500

    let chiantirc actually set gui options

--- chianti/Gui/__init__.py
+++ chianti/Gui/__init__.py
@@ -2,30 +2,38 @@
 trying to get a correct gui package
 '''
 
-#
-hasWx=0
-hasPyQt4=0
-try: 
-    import PyQt4
-    hasPyQt4 = 1
-    print(' found PyQt4 widgets')
-    del PyQt4
-except:
-    try:
-        import wx
-        hasWx = 1
-        print(' found Wx widgets')
-        del wx
-    except:
-        print(' using cli')
-#
-if hasPyQt4:
-    from .gui_qt import gui 
-    print(' using PyQt4 widgets')
-elif hasWx:
-    from .gui_wx import gui
-    print(' using Wx widgets')
-else:
-    from .gui_cl import gui 
+#Get default options from chiantirc
+from chianti.io import defaultsRead
+
+defaults=defaultsRead()
+
+if not defaults['gui']:
+    from .gui_cl import gui
     print(' using CLI for selections')
+else:
+    hasWx=0
+    hasPyQt4=0
+    try: 
+        import PyQt4
+        hasPyQt4 = 1
+        print(' found PyQt4 widgets')
+        del PyQt4
+    except:
+        try:
+            import wx
+            hasWx = 1
+            print(' found Wx widgets')
+            del wx
+        except:
+            print(' using cli')
+    #
+    if hasPyQt4:
+        from .gui_qt import gui 
+        print(' using PyQt4 widgets')
+    elif hasWx:
+        from .gui_wx import gui
+        print(' using Wx widgets')
+    else:
+        from .gui_cl import gui 
+        print(' using CLI for selections')
 

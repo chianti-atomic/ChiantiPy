68e9482fc181a966333cf2a4e1c5593bb4901487
Will Barnes <wtb2@rice.edu>
2016-05-27 00:41:00 -0500

    astropy configuration

---
+++ ChiantiPy/__init__.py
@@ -0,0 +1,17 @@
+'''
+ChiantiPy - CHIANTI Python package
+Calculates various aspects of emission line and continua from the CHIANTI atomic database for astrophysical spectroscopy.
+This is not yet an Astropy affiliated package, but it makes use of the Astropy package template
+'''
+
+# Include this stuff for astropy affiliation
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    # For ChiantiPy
+    from . import  version
+    Version = version.__version__
+    Version_info = version.__version_info__

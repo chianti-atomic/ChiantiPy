eeb27dadde00bfec13de63dc9beee6c9b12baa29
Will Barnes <wtb2@rice.edu>
2016-07-13 00:09:51 -0600

    edited docstrings for blackbody functions and multiprocessing helpers

--- ChiantiPy/tools/mputil.py
+++ ChiantiPy/tools/mputil.py
@@ -1,11 +1,19 @@
 '''
-functions needed for standard Python multiprocessing module mspectrum
+Functions needed for standard Python multiprocessing module mspectrum
 '''
-import ChiantiPy
+
+import ChiantiPy.core as ch
 
 def doFfQ(inQ, outQ):
     '''
-    multiprocessing helper for freefree
+    Multiprocessing helper for `ChiantiPy.core.continuum.freeFree`
+
+    Parameters
+    -----------
+    inQ : `~multiprocessing.Queue`
+        Ion free-free emission jobs queued up by multiprocessing module
+    outQ : `~multiprocessing.Queue`
+        Finished free-free emission jobs
     '''
     for inputs in iter(inQ.get, 'STOP'):
         ionS = inputs[0]
@@ -13,7 +21,7 @@ def doFfQ(inQ, outQ):
         wavelength = inputs[2]
         abund = inputs[3]
         em = inputs[4]
-        ff = chianti.core.continuum(ionS, temperature, abundance=abund, em=em)
+        ff = ch.continuum(ionS, temperature, abundance=abund, em=em)
         ff.freeFree(wavelength)
         outQ.put(ff.FreeFree)
     return
@@ -22,7 +30,14 @@ def doFfQ(inQ, outQ):
     #
 def doFbQ(inQ, outQ):
     '''
-    multiprocessing helper for freeBound
+    Multiprocessing helper for `ChiantiPy.core.continuum.freeBound`
+
+    Parameters
+    -----------
+    inQ : `~multiprocessing.Queue`
+        Ion free-bound emission jobs queued up by multiprocessing module
+    outQ : `~multiprocessing.Queue`
+        Finished free-bound emission jobs
     '''
     for inputs in iter(inQ.get, 'STOP'):
         ionS = inputs[0]
@@ -30,7 +45,7 @@ def doFbQ(inQ, outQ):
         wavelength = inputs[2]
         abund = inputs[3]
         em = inputs[4]
-        fb = chianti.core.continuum(ionS, temperature, abundance=abund, em=em)
+        fb = ch.continuum(ionS, temperature, abundance=abund, em=em)
         fb.freeBound(wavelength)
         outQ.put(fb.FreeBound)
     return
@@ -39,7 +54,14 @@ def doFbQ(inQ, outQ):
     #
 def doIonQ(inQueue, outQueue):
     '''
-    multiprocessing helper for ion, also does two-photon
+    Multiprocessing helper for `ChiantiPy.core.ion` and `ChiantiPy.core.ion.twoPhoton`
+
+    Parameters
+    -----------
+    inQueue : `~multiprocessing.Queue`
+        Jobs queued up by multiprocessing module
+    outQueue : `~multiprocessing.Queue`
+        Finished jobs
     '''
     for inpts in iter(inQueue.get, 'STOP'):
         ionS = inpts[0]
@@ -52,7 +74,7 @@ def doIonQ(inQueue, outQueue):
         abund = inpts[6]
         em = inpts[7]
         doContinuum = inpts[8]
-        thisIon = chianti.core.ion(ionS, temperature, density, abundance=abund)
+        thisIon = ch.ion(ionS, temperature, density, abundance=abund)
         thisIon.intensity(wvlRange = wvlRange, allLines = allLines, em=em)
         if 'errorMessage' not in sorted(thisIon.Intensity.keys()):
             thisIon.spectrum(wavelength,  filter=filter)

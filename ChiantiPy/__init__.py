ae86ba4b168f4e7eaf8b58e5e7a9e61f2c117baa
Will Barnes <wtb2@rice.edu>
2016-07-16 00:39:46 -0600

    Fixing ChianitiPy init; remove version.py; ignore version.py in the future

--- ChiantiPy/__init__.py
+++ ChiantiPy/__init__.py
@@ -1,10 +1,11 @@
 '''
-the ChiantiPy - CHIANTI Python package
-calculates various aspects of emission line and continua from the
-CHIANTI atomic database for astrophysical spectroscopy
+ChiantiPy - CHIANTI Python package
+Calculates various aspects of emission line and continua from the CHIANTI atomic database for astrophysical spectroscopy.
+This is not yet an Astropy affiliated package, but it makes use of the Astropy package template
 '''
-from . import  version
-Version = version.__version__
-Version_info = version.__version_info__
-#
-
+# Include this stuff for astropy affiliation
+from ._astropy_init import *
+if not _ASTROPY_SETUP_:
+    # For ChiantiPy
+    from . import  version
+    Version = version._last_generated_version

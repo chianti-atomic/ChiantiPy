9e4879b1130df752aab46be69292f9c60b376d6f
Will Barnes <wtb2@rice.edu>
2016-11-23 07:08:47 -0600

    fix some versioning stuff, bump version down to 0.6.5 to be consistent with latest PyPI release

--- ChiantiPy/__init__.py
+++ ChiantiPy/__init__.py
@@ -8,16 +8,7 @@ This is not yet an Astropy affiliated package, but it makes use of the Astropy p
 from ._astropy_init import *
 #--------------------------------------------
 
-#if not _ASTROPY_SETUP_:
-#    # For ChiantiPy
-#    from . import  version
-#    Version = version._last_generated_version
-#
-try:
-    from .version import version as __version__
-except ImportError:
-    __version__ = '0.7.0'
-try:
-    from .version import githash as __githash__
-except ImportError:
-    __githash__ = ''
+if not _ASTROPY_SETUP_:
+    # For ChiantiPy
+    from . import  version
+    Version = version._last_generated_version

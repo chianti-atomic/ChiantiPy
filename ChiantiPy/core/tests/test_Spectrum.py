c191b60564d2242e17f0c004cfcecbb50618f9a4
Will Barnes <wtb2@rice.edu>
2016-11-28 15:58:39 -0600

    some very minimal tests for radloss and spectrum classes, i.e. instances are created and that is all.

--- ChiantiPy/core/tests/test_Spectrum.py
+++ ChiantiPy/core/tests/test_Spectrum.py
@@ -1,3 +1,27 @@
 """
-Tests for the spectrum class
+Tests for the spectrum and bunch classes
 """
+
+import numpy as np
+import pytest
+
+from ChiantiPy.core import spectrum,bunch
+
+#set temperature, density, wavelength
+temperature_1 = np.array([1e+6,4e+6,1e+7])
+temperature_2 = np.logspace(5,8,10)
+density = 1e+9
+wavelength = np.linspace(10,100,1000)
+wavelength_range = [wavelength[0],wavelength[-1]]
+min_abund=1.e-4
+ion_list = ['fe_15','fe_16']
+
+# test the spectrum class
+def test_spectrum():
+    _tmp_spec = spectrum(temperature_1,density,wavelength,minAbund=min_abund)
+    # TODO: need to assert something here, not clear what exactly to test yet
+
+# test the bunch class
+def test_bunch():
+    _tmp_bunch = bunch(temperature_2,density,wvlRange=wavelength_range,ionList=ion_list)
+    # TODO: need to assert something here, not clear what exactly to test yet

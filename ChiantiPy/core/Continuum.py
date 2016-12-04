0f9618d1c87d2a1ffc8d242cb7a7c17fdf95adf5
Ken Dere <kdere@gmu.edu>
2016-12-04 12:54:42 -0500

    removed commented out line

--- ChiantiPy/core/Continuum.py
+++ ChiantiPy/core/Continuum.py
@@ -553,7 +553,6 @@ class continuum:
         #
         # for the ionization potential, must use that of the recombined ion
         #
-#        iprcm = self.Ipr/const.invCm2Ev
         #
         # get karzas-latter Gaunt factors
         if hasattr(self, 'Klgfb'):

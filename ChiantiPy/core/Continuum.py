8a4ebe5b2aca491917eafd0a8b5b89cb9016fcdd
Will Barnes <wtb2@rice.edu>
2017-06-29 14:37:17 +0200

    fix dimension mismatch in continuum; fix travis build

--- ChiantiPy/core/Continuum.py
+++ ChiantiPy/core/Continuum.py
@@ -446,7 +446,7 @@ class Continuum(object):
         if include_ioneq:
             fb_emiss *= self.ioneq_one(**kwargs)[:,np.newaxis]
         if self.emission_measure is not None:
-            fb_emiss *= self.emission_measure
+            fb_emiss *= self.emission_measure[:,np.newaxis]
         if ch_data.Defaults['flux'] == 'photon':
             fb_emiss /= photon_energy
         # the final units should be per angstrom

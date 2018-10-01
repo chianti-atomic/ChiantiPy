8411fa0b35a0306a47e52b53871144bac65d2fa3
Ken Dere <kdere@gmu.edu>
2018-10-01 12:18:37 -0400

    trying to get README.md to work on PyPI

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ conf = ConfigParser()
 conf.read(['setup.cfg'])
 metadata = dict(conf.items('metadata'))
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 

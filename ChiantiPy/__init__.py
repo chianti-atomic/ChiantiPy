76e8d4be86e6d81f477a385f7eb1618e861c8385
Ken Dere <kdere@gmu.edu>
2018-10-21 13:36:19 -0400

    imports local version of verion.py

--- ChiantiPy/__init__.py
+++ ChiantiPy/__init__.py
@@ -3,7 +3,6 @@ ChiantiPy - CHIANTI Python package Calculates various aspects of emission lines
 and continua from the CHIANTI atomic database for astrophysical spectroscopy.
 """
 
-from . import version
-Version = version.__version__
-Version_info = version.__version_info__
-
+from . import  version
+__version_info__ = version.__version_info__
+__version__ = version.__version__
